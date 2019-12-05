7a5b2e681b70da0fe4f55193ba652ecf00bdee09
kyungso <cks678928@gmail.com>
2019-12-05 22:08:37 +0900

    react-virtualized as another way how to improve performance

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,10 +7,11 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;
 
     return (
+      <div className="TodoListItem-virtualized" style={style}>
         <div className="TodoListItem">
             <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
@@ -20,7 +21,11 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
                 <MdRemoveCircleOutline onClick={() => onRemove(id)} />
             </div>
         </div>
+      </div>
     );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(
+    TodoListItem,
+    (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
